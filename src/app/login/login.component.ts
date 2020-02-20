9454b79c98a2dfb45badcaf7746403d94391b996
WilliamMelgoza <wmelgoza@protonmail.com>
2020-02-19 18:51:49 -0600

    Create and test

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../services/user.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) {
+    console.log(this.userService.test());
+  }
 
   ngOnInit(): void {
   }
